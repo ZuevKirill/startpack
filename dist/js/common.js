e5402252efae3df6488c8cfee2983f65ccb14364
Кирилл Зуев <kirillzuev@MacBook-Pro-Kirill.local>
2017-06-13 13:33:32 +0500

    add owlcarousel

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,5 +1,13 @@
-$(function() {
-
-	// Пользовательские функции
-
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,8 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 gulp.task('scripts', function() {
 	return gulp.src([ // Берем все необходимые библиотеки
 		//'app/libs/**/*.min.js', // грузим все
-		'app/libs/jquery/dist/jquery.min.js' // Берем jQuery
+		'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
+		'app/libs/owl.carousel/dist/owl.carousel.js'
 		])
 		.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
 		.pipe(uglify()) // Сжимаем JS файл
